720062b601580ec3a84230d1fe6c9cc8d4a79602
Ken Warren <134076996+ken-warren@users.noreply.github.com>
2025-01-10 22:31:10 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-
+/*================ scroll reveal =========== */
+const typed = new Typed('.multiple-text', {
+    strings: ['Custom Tailoiring Services', 'Boutique Sales', 'Fabric and Design Advisory'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+});
