e63463220e7c32c1ce5a688e51317eabaee86414
stella160 <stellamarriessyombua@gmail.com>
2025-01-18 13:52:10 +0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,21 @@
 /*================ scroll reveal =========== */
+
+ScrollReveal ({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay:200
+});
+
+ScrollReveal().reveal('.home-container, h2, .pro-container img', { origin: 'top' });
+ScrollReveal().reveal('#home, .home-container p, .home-container button', { origin: 'bottom' });
+ScrollReveal().reveal('.home-container h1, .des', { origin: 'left' });
+ScrollReveal().reveal('.home-container h, .pro-container', { origin: 'right' });
+
+
+/*================ typed Text =========== */
 const typed = new Typed('.multiple-text', {
-    strings: ['Custom Tailoiring Services', 'Boutique Sales', 'Fabric and Design Advisory'],
+    strings: ['Custom Tailoring Services', 'Boutique Sales', 'Fabric and Design Advisory'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
