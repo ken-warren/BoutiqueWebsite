adb2ad6a1f37660298a10cf51eb9b17b51059fb3
kenwarrenzioks@gmail.com <kenwarrenzioks@gmail.com>
2025-01-26 01:14:25 +0300

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,33 @@
 /*================ scroll reveal =========== */
-
-ScrollReveal ({
-    reset: true,
-    distance: '80px',
-    duration: 2000,
-    delay:200
+ScrollReveal({
+  reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
 });
 
-ScrollReveal().reveal('.home-container, h2, .pro-container img', { origin: 'top' });
-ScrollReveal().reveal('#home, .home-container p, h3, .home-container button', { origin: 'bottom' });
-ScrollReveal().reveal('.home-container h1, .des', { origin: 'left' });
-ScrollReveal().reveal('.home-container h, .pro-container', { origin: 'right' });
-
+ScrollReveal().reveal(
+  ".home-container, h2, .pro-container img, .newsletter-form, #ankara img",
+  { origin: "top" }
+);
+ScrollReveal().reveal(
+  "#home, .home-container p, h3, .home-container button, footer p",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(
+  "#home, .home-container p, h3, .home-container button, footer p",
+  { origin: "left" }
+);
+ScrollReveal().reveal(
+  ".home-container h, .pro-container, .ankara-gallery, .newsletter-form button",
+  { origin: "right" }
+);
 
 /*================ typed Text =========== */
-const typed = new Typed('.multiple-text', {
-    strings: ['Tailoring Services', 'Boutique Sales', 'Kitenge Designs'],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop:true
+const typed = new Typed(".multiple-text", {
+  strings: ["Tailoring Services", "Boutique Sales", "Kitenge Designs"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
 });
