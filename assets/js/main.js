cf7f4a28d0c97888a6c62b7bf3d12479d963f7d6
kenwarrenzioks@gmail.com <kenwarrenzioks@gmail.com>
2025-02-10 00:04:08 +0300

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,3 +31,41 @@ const typed = new Typed(".multiple-text", {
   backDelay: 1000,
   loop: true,
 });
+
+/* Highlight Animation*/
+
+function showHighlight(selected) {
+  let highlights = document.querySelectorAll(".highlight");
+
+  highlights.forEach((highlight) => {
+    if (highlight === selected) {
+      highlight.classList.add("active");
+    } else {
+      highlight.classList.remove("active");
+    }
+  });
+}
+
+/* Fast Video & Controls*/
+document.addEventListener("DOMContentLoaded", function () {
+  const video = document.getElementById("highlightVid");
+  const playBtn = document.getElementById("playBtn");
+  const pauseBtn = document.getElementById("pauseBtn");
+
+  video.playbackRate = 2.0;
+
+  video.pause();
+  pauseBtn.style.display = "none";
+
+  playBtn.addEventListener("click", () => {
+    video.play();
+    playBtn.style.display = "none";
+    pauseBtn.style.display = "inline-block";
+  });
+
+  pauseBtn.addEventListener("click", () => {
+    video.pause();
+    pauseBtn.style.display = "none";
+    playBtn.style.display = "inline-block";
+  });
+});
